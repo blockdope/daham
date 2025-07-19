8ce823560b8925e0699dc03dcde9dbc993c2558f
sayuru313 <service.sayuru@gmail.com>
2025-07-19 14:01:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "CFZyHZjB#i8TtKtMLDW9Z-u-_9GN2KoMi1HBynzvTvZZ6OMx7xzY", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
